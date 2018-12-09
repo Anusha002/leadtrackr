3f85396e0651442963660161c3c3fa1e13ace125
Anish Vishwanathan <anish.vishwanathan@paytmmall.com>
2018-12-09 08:10:28 +0530

    Styling of login page

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,13 @@
 .page__background { 
   background: linear-gradient(135deg, rgba(8,179,222,1) 10%, rgba(8,179,222,1) 37%, rgba(8,179,222,1) 45%, rgba(5,37,199,1) 100%);
 }
+.lead-input{
+  width: 100%;
+}
+.text-danger{
+  font-size: .5em;
+  color: #D91045;
+  margin: 0 2px;
+}
 
 </style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,21 +1,25 @@
 <template>
   <v-ons-page>
+    <div class="logo">
+      <img :src="require('../assets/lead-logo.png')" width="90"  />
+      <h4>LeadTrackr</h4>
+    </div>
    <div class="logindetails">
 
-    <v-ons-list>
-	    <v-ons-list-item>
+    <v-ons-list class="box-list">
+	    <v-ons-list-item modifier="nodivider">
 	      <div class="center">
-	        <v-ons-input type="text" name="username" placeholder="User name" v-model="input.username" v-validate="'required|min:3'" ></v-ons-input>
+	        <v-ons-input type="text" modifier="underbar" class="lead-input" name="username" placeholder="User name" v-model="input.username" v-validate="'required|min:3'" ></v-ons-input>
           <p class="text-danger" v-if="errors.has('username')">{{ errors.first('username')}}</p>   
 	      </div>
 	    </v-ons-list-item>
-	    <v-ons-list-item>
+	    <v-ons-list-item modifier="nodivider">
 	    	<div class="center">
-	    		<v-ons-input type="password" name="password" placeholder="Password" v-model="input.password" v-validate="'required'" ></v-ons-input>
+	    		<v-ons-input type="password" modifier="underbar" class="lead-input"  name="password" placeholder="Password" v-model="input.password" v-validate="'required'" ></v-ons-input>
           <p class="text-danger" v-if="errors.has('password')">{{ errors.first('password')}}</p>  
 	    	</div>
 	    </v-ons-list-item>
-	    <v-ons-list-item>
+	    <v-ons-list-item modifier="nodivider">
 	    	<v-ons-button modifier="large" style="margin: 6px 0" @click="sendData()">Login</v-ons-button>
 	    </v-ons-list-item>
 	</v-ons-list>
@@ -67,6 +71,15 @@ export default {
 	background-color: #fff !important;
 
 }
+.logo{
+  position: relative;
+  left: 50%;
+  margin-left: -45px;
+  top: 40px;
+}
+.logo h4 {
+  color: #fff;
+}
 
 .logindetails {
   border: 1px;
@@ -74,5 +87,10 @@ export default {
   border-radius: 6px;
 }
 
-
+.box-list {
+  border-radius: 10px;
+}
+.box-list .list-item {
+  padding-right: 8px;
+}
 </style>      
