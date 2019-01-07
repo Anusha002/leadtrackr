0b45b92000f8ba21a26b7f0b9a2a4ca235a61d26
Anish Vishwanathan <anishkv@gmail.com>
2019-01-07 11:18:25 +0530

    addfollowup

--- src/pages/Addfollowup.vue
+++ src/pages/Addfollowup.vue
@@ -8,12 +8,13 @@
         		</div>
         	</v-ons-list-item>
           <v-ons-list-item>
-           <v-ons-select style="width: 100%" v-model="followup.Stage">
-                <option value="" selected data-default></option>
+           <v-ons-select style="width: 100%" v-model="followup.Stage" name="stage" v-validate="'required'">
+                  <option value="" selected ></option>
                 <option v-for="item in stages" :value="item">
                   {{ item }}
                 </option>
             </v-ons-select>
+             <p class="text-danger" v-if="errors.has('stage')">{{ errors.first('stage')}}</p> 
           </v-ons-list-item>
           <v-ons-list-item>
            <div class="followupdetails">
@@ -22,29 +23,31 @@
           </v-ons-list-item>
           <v-ons-list-item>
            <div class="followupdetails">
-            <v-ons-input placeholder="Task" v-model="followup.Task"></v-ons-input>
+            <v-ons-input placeholder="Task" v-model="followup.Task" name="task" type="text" v-validate="'required'"></v-ons-input>
+            <p class="text-danger" v-if="errors.has('task')">{{ errors.first('task')}}</p> 
             </div>
           </v-ons-list-item>
           <v-ons-list-item>
            <div class="followupdetails">
             
-            <v-date-picker mode='single'v-model="followup.FollowupDate">
+            <v-date-picker mode='single'v-model="followup.FollowupDate" name="followupdate" v-validate="'required'">
+              <p class="text-danger" v-if="errors.has('followupdate')">{{ errors.first('followupdate')}}</p> 
              </v-date-picker>
             </div>
 
           </v-ons-list-item>
           <v-ons-list-item> 
-             <v-ons-select style="width: 100%" v-model="followup.ScheduleBy">
+             <v-ons-select style="width: 100%" v-model="followup.ScheduleBy" name="scheduleby" v-validate="'required'">
                <option value="" selected data-default></option>
-                <option v-for="(value,key) in scheduleBy" :value="value.UserID">
+                <option v-for="(value,key) in scheduleBy" :value="value.FullName">
                   {{ value.FullName }}
                 </option>
             </v-ons-select>
           </v-ons-list-item>  
           <v-ons-list-item> 
-             <v-ons-select style="width: 100%" v-model="followup.ScheduleTo">
+             <v-ons-select style="width: 100%" v-model="followup.ScheduleTo" name="scheduleto" v-validate="'required'">
                 <option value="" selected data-default></option>
-                <option v-for="(value,key) in scheduleTo" :value="value.UserID">
+                <option v-for="(value,key) in scheduleTo" :value="value.FullName">
                    {{ value.FullName }}
                 </option>
             </v-ons-select>
@@ -55,7 +58,7 @@
             </div>
           </v-ons-list-item>
           <v-ons-list-item> 
-             <v-ons-select style="width: 100%" v-model="followup.Status">
+             <v-ons-select style="width: 100%" v-model="followup.Status" name="status" v-validate="'required'">
                 <option value="" selected data-default></option>
                 <option v-for="item in status" :value="item">
                   {{ item }}
@@ -64,7 +67,7 @@
           </v-ons-list-item>  
           <v-ons-list-item>
           <v-ons-button  style="margin: 6px 4px" @click="goToHome()">Cancel</v-ons-button>
-          <v-ons-button  style="margin: 6px 4px" @click="addFollowup()">Save</v-ons-button>
+          <v-ons-button  style="margin: 6px 4px" @click="addFollowup(items.ProjectID)">Save</v-ons-button>
         </v-ons-list-item>           
        </v-ons-list>
   </v-ons-page>   
@@ -81,18 +84,17 @@ export default{
     data() {
     return {
       followup: {
-      ProjectID: "",
-      Stage: "", 
-      UserID:"",
-      Task:"",
-      CreatedDate: new Date(),
-      FollowupDate: "",
-      ScheduleBy: "", 
-      ScheduleTo: "",
-      Description:"",
-      CreatedBy:"",
-      Status:"",
-      Token:localStorage.ki 
+        ProjectID: "",
+        Stage: "", 
+        UserID:"",
+        Task:"",
+        CreatedDate: new Date(),
+        FollowupDate: "",
+        ScheduleBy: "", 
+        ScheduleTo: "",
+        Description:"",
+        Status:"",
+        Token:localStorage.ki 
       },
       stages:["Select Stage"],
       status:["Select Status"],
@@ -106,30 +108,36 @@ export default{
         this.$router.push('/followups')
       },
 
-    addFollowup(){
-      
+    addFollowup(pid){
+      this.followup.ProjectID = pid.toString();
       this.submitted = true;
       var data = this.followup;
       this.followup.CreatedDate = Utils.formatDate(new Date())
       this.followup.FollowupDate = Utils.formatDate(new Date(data.FollowupDate))
-      AddfollowupApi.addFollowup(data).then(followups => {
+      
+      this.$validator.validate().then(valid => {
 
-        console.log("followup added successfully");
-        this.$router.push('/followups');
+        if (valid) {
+            AddfollowupApi.addFollowup(data).then(followups => {
 
-    }, error => {
-        console.error(error);
-       }); 
-     }
-  },
+            this.$router.push('/followups');
+
+            }, error => {
+               console.error(error);
+            }); 
+          }
+        })
+      }
+    },
    mounted:function() {
+
     var payload = {
           Token:localStorage.ki
 
          }
 
      var user =  JSON.parse(localStorage.usr);
-     this.followup.UserID = user.UserID; 
+     this.followup.UserID = Utils.getUserid(); 
 
     Utils.getStatus(payload).then(status => {
       this.status = this.status.concat(status.Body);
@@ -137,6 +145,8 @@ export default{
 
     Utils.getStage(payload).then(stage => {
       this.stages = this.stages.concat(stage.Body);
+      // this.stages = stage.Body;
+      
       
     }),
     Utils.getUser(payload).then(users => {
