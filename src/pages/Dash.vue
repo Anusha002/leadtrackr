de243dfecb9f30e185a2b62c7d48f0bb88686499
Anish Vishwanathan <anishkv@gmail.com>
2019-02-03 09:02:53 +0530

    card issue fixed

--- src/pages/Dash.vue
+++ src/pages/Dash.vue
@@ -251,12 +251,8 @@ export default {
 #dash .c-title br{
 	display: none;
 }
-.task-card {
-	width: 310px !important;
-	margin-left: 5px;
-	margin-right:19px;
-	
-}
+
+
 .tasknumbers{
 	font-size:16px;
 	font-weight: bold;
